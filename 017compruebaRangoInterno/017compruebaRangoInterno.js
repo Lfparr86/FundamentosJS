da2d7b77e071eab2de5732a693c74d257519fe6d
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2022-11-02 12:58:49 +0100

    actualizacion 17

---
+++ 017compruebaRangoInterno/017compruebaRangoInterno.js
@@ -0,0 +1,8 @@
+
+let edadParaComprobar = prompt ("¿Que edad tienes?");
+
+if (edadParaComprobar >= 18 && edadParaComprobar <=99) {
+    alert("La edad esta entre 18 y 99");
+}else{
+    alert ("La edad esta fuera del rango de 18 y 99");
+}
